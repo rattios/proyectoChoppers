fc2e5fe7eb172c0b6352bea252df12ffbc1a35a2
stalinrangel <e.stalinrangel@gmail.com>
2018-07-22 18:28:18 -0430

    stalin2

--- Angular4_CLI_Full_Project/src/app/app.component.ts
+++ Angular4_CLI_Full_Project/src/app/app.component.ts
@@ -10,8 +10,9 @@ export class AppComponent {
 
 	constructor(private permissionsService: NgxPermissionsService) { 
     	console.log(localStorage.getItem('shoppers_permis'));
-    	if (localStorage.getItem('shoppers_permis') !== '') {
+    	if (localStorage.getItem('shoppers_permis') != '' ) {
     		var perm2 = JSON.parse(localStorage.getItem('shoppers_permis'));
+        //const perm2 = ["EMPRESA"];
 	    	this.permissionsService.loadPermissions(perm2);
     	}
   	}
