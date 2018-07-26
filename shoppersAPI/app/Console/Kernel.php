c7c86827008fb5f78ef209fccf087f8e7bbad4bb
freddy gabriel ramirez <freddy gabriel ramirez>
2018-07-26 16:44:36 -0400

    api - tareas automaticas

--- shoppersAPI/app/Console/Kernel.php
+++ shoppersAPI/app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\Inspire::class,
+        Commands\NotificationClientes::class,
     ];
 
     /**
@@ -26,5 +27,8 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inspire')
                  ->hourly();
+
+        $schedule->command('clientes:notificar')
+                 ->everyMinute();
     }
 }
